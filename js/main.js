3cb6dc6b31072cb4a1da83e85e16b75ad359d4db
elchinab <elchinabdurahman10@gmail.com>
2022-03-16 16:19:50 +0400

    a

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    "use strict"
+    $("#header").ripples({
+        resolution: 512,
+        dropRadius: 20,
+        perturbance: 0.03,
+        
+      })
+
+     
+});
